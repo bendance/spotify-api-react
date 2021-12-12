1c47f6df60fc95a1b0261124875a82093c5c956d
bendance <bendance@iu.edu>
2021-12-12 18:32:00 -0500

    API now responds appropriately

--- myapp/demo/src/MyForm.js
+++ myapp/demo/src/MyForm.js
@@ -6,20 +6,28 @@ class MyForm extends React.Component
     {
         super(props);
         this.state = { name: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange = (event) => 
     {
-        this.setState({[event.target.name]: event.target.value});
+        this.setState({ name: event.target.value });
     }
 
     handleSubmit = (event) =>
     {
-        alert('A form was submitted: ' + this.state);
+        alert('A form was submitted: ' + this.state.name);
+        
+        console.log(JSON.stringify(this.state))
 
         fetch('http://localhost:8080/store-data', {
             method: 'POST',
-            body: JSON.stringify(this.state)
+            body: JSON.stringify(this.state),
+            headers: {
+                "Content-Type": "application/json"
+            },
         }).then(function(response)
         {
             console.log(response);
@@ -35,7 +43,7 @@ class MyForm extends React.Component
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Name:
-                    <input type = "text" value = {this.state.value} name = "name" onChange={this.handleChange} />
+                    <input type = "text" value = {this.state.name} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
--- myapp/index.js
+++ myapp/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const port = process.env.port || 8080;
 
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 });
 
-app.post("/post", (req, res) => {
-    console.log(req);
-    res.redirect("/");
+app.post("/store-data", (req, res) => {
+    let name = req.body.name;
+    console.log(req.body);
+    res.send(`Hi ${name}!`);
 });
 
 app.listen(port, () => {
