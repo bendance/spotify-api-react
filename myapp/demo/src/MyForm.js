89b129e8c63c0835fd571d1c7bdabed379cc016e
bendance <bendance@iu.edu>
2021-12-15 20:42:14 -0500

    got basic input for form set up

--- myapp/demo/src/MyForm.js
+++ myapp/demo/src/MyForm.js
@@ -6,16 +6,36 @@ class MyForm extends React.Component
     {
         super(props);
         this.state = { 
-            userID: '',
-            accessToken: ''};
+            playlistName: '',
+            playlistPublic: '',
+            playlistDescription: '',
+            userFavoriteGenre: ''};
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangePlaylistName = this.handleChangePlaylistName.bind(this);
+        this.handleChangePlaylistPublic = this.handleChangePlaylistPublic.bind(this);
+        this.handleChangePlaylistDescription = this.handleChangePlaylistDescription.bind(this);
+        this.handleChangeUserFavoriteGenre = this.handleChangeUserFavoriteGenre.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange = (event) => 
+    handleChangePlaylistName = (event) => 
     {
-        this.setState({ userID: event.target.value });
+        this.setState({ playlistName: event.target.value });
+    }
+
+    handleChangePlaylistPublic = (event) =>
+    {
+        this.setState({ playlistPublic: event.target.value });
+    }
+
+    handleChangePlaylistDescription = (event) =>
+    {
+        this.setState({ playlistDescription: event.target.value });
+    }
+
+    handleChangeUserFavoriteGenre = (event) => 
+    {
+        this.setState({ userFavoriteGenre: event.target.value });
     }
 
     handleSubmit = (event) =>
@@ -36,16 +56,29 @@ class MyForm extends React.Component
     render() 
     {
         return(
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Username: 
-                        <input type = "text" value = {this.state.userID} onChange={this.handleChange} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                    <br></br>
-                </form>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label for= {this.state.playlistName}>Playlist Name: </label>
+                <br></br>
+                <input type = "text" value = {this.state.playlistName} onChange={this.handleChangePlaylistName} />
+                <br></br>
+                <label for = {this.state.playlistDescription}>Playlist Description: </label>
+                <br></br>
+                <input type = "text" value = {this.state.playlistDescription} onChange={this.handleChangePlaylistDescription} />
+                <br></br>
+                <label for = {this.state.userFavoriteGenre}>Favorite Genre: </label>
+                <br></br>
+                <select name = {this.state.userFavoriteGenre} id = {this.state.userFavoriteGenre} onChange={this.handleChangeUserFavoriteGenre}>
+                    <option value = "Pop">Pop</option>
+                    <option value = "Hip Hop">Hip Hop</option>
+                    <option value = "Rock">Rock</option>
+                    <option value = "Electronic">Electronic</option>
+                    <option value = "Latin">Latin</option>
+                </select>
+                <br></br>
+                Would you like to make your playlist public? 
+                <input type = "checkbox" id = "Playlist Public" onChange={this.handleChangePlaylistPublic} />
+                <br></br>
+            </form>
         );
     }
 }
