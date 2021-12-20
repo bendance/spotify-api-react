56fd9924886289c8d68f8de2c81a8c60ccd91744
bendance <bendance@iu.edu>
2021-12-20 00:31:22 -0500

    updated success screen

--- myapp/demo/src/Success.js
+++ myapp/demo/src/Success.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function Success(props)
 {
+    const openPlaylist = () => {
+        window.open(
+            "https://www.apple.com/", 
+            '_blank'
+        );
+    }
+
     return(
-        <h2 className="text-center">Your playlist has been successfully created!</h2>
+        <div className="text-center">
+            <h2 className="text-center">Your playlist has been successfully created!</h2>
+            <div className = "d-flex">
+                <button type = "button" className = "btn btn-primary" onClick={openPlaylist}>Go To Playlist</button>
+                <button type = "button" className = "btn btn-primary">Create Another</button>
+            </div>
+        </div>
     )
 }
 
